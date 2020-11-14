a09a4a5416e5fc2eaf9377563b15fb8ec13f8bb0
WouterDeveloper <wouterwijnands@protonmail.com>
2020-11-14 11:23:06 +0100

    Created basics of program. Added laraval and webpack and basic js and sass files

---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+// Later imports verwerken
+//import { hello } from './module.mjs';
+
+
+  window.onload = function() {
+    // document.querySelectorAll('.skills__block .skills__header')
+    // .forEach(function(el, i) {
+    //     el.className += ' header'+(i+1);
+    // })
+
+    // document.querySelectorAll('.skills__techniques__row figure')
+    // .forEach(function(el, i) {
+    //     el.className += ' figure'+(i+1);
+    // })
+  //  var hamburger = document.querySelector(".hamburger");
+  //  var navigation = document.querySelector(".content nav ul");
+
+  //  hamburger.addEventListener("click", function() {
+  //    hamburger.classList.toggle("is-active");
+  //    navigation.classList.toggle("is-active");
+  //  });
+  };
+
